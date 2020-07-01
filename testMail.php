51a7dbdf8e0976aa124b485a64d04257c2fa600a
Claudio Christyo <mosint.nagant@gmail.com>
2020-07-01 11:33:43 +0700

    changed credentials for email

--- testMail.php
+++ testMail.php
@@ -41,16 +41,16 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'claudio.christyo@gmail.com';                     // SMTP username
-    $mail->Password   = 'thraxelonrules50124aaa';                               // SMTP password
+    $mail->Username   = 'testmailphh@gmail.com';                     // SMTP username
+    $mail->Password   = 'mailtestphh501';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('claudio.christyo@gmail.com', 'Claudio');
+    $mail->setFrom('testmailphh@gmail.com', 'Sender');
     #$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
     $mail->addAddress($recipient);               // Name is optional
-    $mail->addReplyTo('claudio.christyo@gmail.com', 'Claudio');
+    $mail->addReplyTo('testmailphh@gmail.com', 'Sender');
     #$mail->addCC('cc@example.com');
     #$mail->addBCC('bcc@example.com');
 
