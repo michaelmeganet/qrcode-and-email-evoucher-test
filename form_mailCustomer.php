94d30f2940dd59e802e2d41dbb4595877bfc60d9
Claudio Christyo <mosint.nagant@gmail.com>
2020-07-06 13:59:16 +0700

    Changed code to accomodate Class CREATE_E_VOUCHER

--- form_mailCustomer.php
+++ form_mailCustomer.php
@@ -1,4 +1,5 @@
 <?php
+namespace voucher\EVoucher;
 session_start();
 if(isset($_SESSION['mailMsg'])){
     $mailMsg = $_SESSION['mailMsg'];
@@ -34,20 +35,26 @@ include 'header.php';
     ?>
     </label>
     <form action='testMail.php' method='POST'>
+        <div class="form-group row row-no-gutters">
+            <div class="col-sm-1">                     
+                <label class="label label-default">User :</label><br> 
+                <input class='form-control text-primary ' style="text-align: center;padding-right: 3px;padding-left:3px;width:auto"type="text" name="userid" id='userid' value="" placeholder="userid" maxlength="10" />
+            </div>
+        </div>
         <div class="form-group row">
             <div class='container'>
                 <label class="label label-default">Who to send?</label><br>
             </div>
             <div class="col-sm-3">
-                <input class='form-control text-primary' type="text" value ='who@address.com' name='address' id='address'/>
+                <input class='form-control text-primary' type="text" value ='' name='address' id='address' placeholder="who@address.com"/>
             </div>
             <div class="col-sm-3">
-                <input class='form-control text-primary' type='text' value ='customer name' name="customer_name" id='customer_name'/><br>
+                <input class='form-control text-primary' type='text' value ='' name="customer_name" id='customer_name' placeholder="customer name"/><br>
             </div>
         </div>
         <div class='container form-group row'>
             <label class='label label-default'>Voucher Type :</label>
-            <label class='radio-inline'><input type='radio' name='radioRM' id='radioRM' value='5'/>RM 5</label>
+            <label class='radio-inline'><input type='radio' name='radioRM' id='radioRM' value='5' checked="checked"/>RM 5</label>
             <label class='radio-inline'><input type='radio' name='radioRM' id='radioRM' value='10'/>RM 10</label>
         </div>
         <div class="container form-group row" style="text-align: right">
--- testMail.php
+++ testMail.php
@@ -1,35 +1,9 @@
 <?php
+namespace voucher\EVoucher;
 
-session_start();
 include_once 'generateinstanceid.php';
 
-if (isset($_POST)){
-    $recipient = $_POST['address'];
-    $voucheramount = $_POST['radioRM'];
-    $_SESSION['post'] = $_POST;
-    #print_r($_SESSION['post']);
-    $userid1 = 'claudio';
-    $arr_getRunno = generate_runno($userid1,$voucheramount);
-    $tID = $arr_getRunno['instancetid'];
-    $datecreate = $arr_getRunno['datecreate'];
-    $expiredate = $arr_getRunno['expiredate'];
-    //get the scriptfilename
-    $encode_ID = urlencode($tID);
-    $qrURL = "qrcodeimage.php?code=$encode_ID";
-    $currURL = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-    $curr = str_replace(basename((__FILE__)),$qrURL,$currURL);
-    echo "curr = ".$curr."<br>";
-
-    //Create PNG of Barcode
-    $output = file_get_contents($curr);
-    file_put_contents('./qrcode_img.png', $output);
-        
-    //create PDF for the E-Voucher
-    //include_once 'printToPDF.php';
-    
-    //directory of PDF file
-    //$evoucher = './pdf/evoucher_tmp.pdf';
-}
+use CREATE_E_VOUCHER;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -38,60 +12,104 @@ require './assets/PHPMailer/src/Exception.php';
 require './assets/PHPMailer/src/PHPMailer.php';
 require './assets/PHPMailer/src/SMTP.php';
 
-$mail = new PHPMailer(true);
-/**/
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'meganet003@gmail.com';                     // SMTP username
-    $mail->Password   = 'mega6636';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+session_start();
 
-    //Recipients
-    $mail->setFrom('meganet003@gmail.com', '-noreply');
-    #$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-    $mail->addAddress($recipient);               // Name is optional
-    $mail->addReplyTo('meganet003@gmail.com', '-noreply');
-    $mail->addEmbeddedImage('./qrcode_img.png', 'qrcode'); //Adds an image to be embedded
-    #$mail->addCC('cc@example.com');
-    #$mail->addBCC('bcc@example.com');
+if (isset($_POST)){
+    $recipient = $_POST['address'];
+    $voucheramount = $_POST['radioRM'];
+    $userid = $_POST['userid'];
+    $_SESSION['post'] = $_POST;
+    #print_r($_SESSION['post']);
+}else{
+    die("Please try <a href='index.php'>again</a>.");
+}  
 
-    // Attachments
-    #$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    #$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-    #$mail->addAttachment($evoucher);
-    
-    //creating body for message
-    ob_start();
-    include('testBody.php');
-    $body_html = ob_get_contents();
-    ob_get_clean();
-    ob_end_clean();
-    
- 
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Test Message, QRCode in mail body, without PDF Attachment';
-    $mail->Body = $body_html;
-    #$mail->Body    = "This is a test Message!<br>"
-    #               . "You got a voucher of $vouchertype!<br>"
-    #                . "Scan This please !<br>"
-    #                ."<img src='qrcodeimage.php?code=$text_encode'/>";
-    #$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+try{
+    $datecreate = date('Y-m-d H:i:s');
+    $objEVoucher = new CREATE_E_VOUCHER($userid, $voucheramount, $datecreate);
+    $result = $objEVoucher->create_voucher();
+    echo "\$result  =$result<br>";
+    if ($result != 'Insert Successful!'){
+        throw new Exception("Failed to insert.");
+    }else{
+        $instanceid = $objEVoucher->get_instanceid();
+        $expiredate = $objEVoucher->get_expiredate();
+        
+        //create script filename
+        $encode_ID = urlencode($instanceid);
+        $qrURL = "qrcodeimage.php?code=$encode_ID";
+        $currURL = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+        $curr = str_replace(basename((__FILE__)),$qrURL,$currURL);
+        echo "curr = ".$curr."<br>";
+       
+        //Create PNG of Barcode
+        $output = file_get_contents($curr);
+        file_put_contents('./qrcode_img.png', $output);
 
-    $mail->send();
-    $_SESSION['mailStat'] = 'success';
-    $mailMsg = "Message has been sent to $recipient";
-} catch (Exception $e) {
+        //create PDF for the E-Voucher
+        //include_once 'printToPDF.php';
+
+        //directory of PDF file
+        //$evoucher = './pdf/evoucher_tmp.pdf';
+
+        $mail = new PHPMailer(true);
+        /**/
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'meganet003@gmail.com';                     // SMTP username
+            $mail->Password   = 'mega6636';                               // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+            //Recipients
+            $mail->setFrom('meganet003@gmail.com', '-noreply');
+            #$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+            $mail->addAddress($recipient);               // Name is optional
+            $mail->addReplyTo('meganet003@gmail.com', '-noreply');
+            $mail->addEmbeddedImage('./qrcode_img.png', 'qrcode'); //Adds an image to be embedded
+            #$mail->addCC('cc@example.com');
+            #$mail->addBCC('bcc@example.com');
+
+            // Attachments
+            #$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            #$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+            #$mail->addAttachment($evoucher);
+
+            //creating body for message
+            ob_start();
+            include('testBody.php');
+            $body_html = ob_get_contents();
+            ob_get_clean();
+            ob_end_clean();
+
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Test Message, QRCode in mail body, without PDF Attachment';
+            $mail->Body = $body_html;
+            #$mail->Body    = "This is a test Message!<br>"
+            #               . "You got a voucher of $vouchertype!<br>"
+            #                . "Scan This please !<br>"
+            #                ."<img src='qrcodeimage.php?code=$text_encode'/>";
+            #$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            $_SESSION['mailStat'] = 'success';
+            $mailMsg = "Message has been sent to $recipient";
+        } catch (Exception $e) {
+            $_SESSION['mailStat'] = 'error';
+            $mailMsg = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+            $_SESSION['mailMsg'] = $mailMsg;
+    }
+
+} catch (Exception $ex) {
     $_SESSION['mailStat'] = 'error';
-    $mailMsg = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    $_SESSION['mailMsg'] = $ex->getMessage();
 }
+echo '<META HTTP-EQUIV="refresh" content="0;URL=form_mailCustomer.php">'; //using META tags instead of headers because headers didn't work in PHP5.3
 
-    $_SESSION['mailMsg'] = $mailMsg;
-    echo '<META HTTP-EQUIV="refresh" content="0;URL=form_mailCustomer.php">'; //using META tags instead of headers because headers didn't work in PHP5.3
-         
 /**/
