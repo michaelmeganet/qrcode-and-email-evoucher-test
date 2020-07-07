739edb7192761071646bb8c88a282ad6f806ef6c
michaelmeganet <phhfirewall@gmail.com>
2020-07-07 23:09:12 +0800

    add debug function to check error

--- form_redeemVoucher.php
+++ form_redeemVoucher.php
@@ -1,24 +1,26 @@
 <?php
+
 namespace voucher\Validate;
+
 session_start();
 include 'header.php';
 $physvoucherRadio = '';
 $evoucherRadio = '';
 
-if(isset($_POST['userid'])){
+if (isset($_POST['userid'])) {
     $userid = $_POST['userid'];
     unset($_POST['userid']);
 }
-if(isset($_POST['vouchertype'])){
+if (isset($_POST['vouchertype'])) {
     $vouchertype = $_POST['vouchertype'];
-    if($vouchertype=='evoucher'){
-        $evoucherRadio = "checked='checked'";   
-    }else{
+    if ($vouchertype == 'evoucher') {
+        $evoucherRadio = "checked='checked'";
+    } else {
         $physvoucherRadio = "checked='checked'";
     }
 }
 
-if(isset($_SESSION['VOUCHER_UPD_MSG'])){
+if (isset($_SESSION['VOUCHER_UPD_MSG'])) {
     $voucherUpdMsg = $_SESSION['VOUCHER_UPD_MSG'];
     session_destroy();
 }
@@ -34,56 +36,58 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <body>
+    <body>
         <div class='container'>
             <h3 class="text-primary">Validate Voucher</h3>
             <form action="" method="POST">
                 <div class="form-group row row-no-gutters">
-                    <div class="col-sm-1">                     
-                        <label class="label label-default">User :</label><br> 
-                        <input class='form-control text-primary ' style="text-align: center;padding-right: 3px;padding-left:3px;width:auto"type="text" name="userid" id='userid' value="<?php if(isset($userid)){ echo $userid; } ?>" placeholder="userid" maxlength="10" />
+                    <div class="col-sm-1">
+                        <label class="label label-default">User :</label><br>
+                        <input class='form-control text-primary ' style="text-align: center;padding-right: 3px;padding-left:3px;width:auto"type="text" name="userid" id='userid' value="<?php if (isset($userid)) {
+    echo $userid;
+} ?>" placeholder="userid" maxlength="10" />
                     </div>
                 </div>
                 <div class="form-group row row-no-gutters">
                     <div class="col-sm-5">
                         <label class="label label-default">Voucher Type :</label>
                         <div class='row-no-gutters'>
-                            <label class=" radio-inline "><input type="radio" name="vouchertype" id='vouchertype' value="preprintvoucher" <?php echo $physvoucherRadio;?>/>Physical Voucher</label>
-                            <label class=" radio-inline "><input type="radio" name="vouchertype" id='vouchertype' value="evoucher" <?php echo $evoucherRadio;?> />E-Voucher</label> 
+                            <label class=" radio-inline "><input type="radio" name="vouchertype" id='vouchertype' value="preprintvoucher" <?php echo $physvoucherRadio; ?>/>Physical Voucher</label>
+                            <label class=" radio-inline "><input type="radio" name="vouchertype" id='vouchertype' value="evoucher" <?php echo $evoucherRadio; ?> />E-Voucher</label>
                             <input class="inline btn btn-info " style="padding:3px 5px 3px 5px" type='submit' name='submitVoucherType' id='submitVoucherType' value='Select' />
                         </div>
                     </div>
                 </div>
             </form>
-        <?php
-        if (isset($vouchertype)){
-                ?>
-            <form action='<?php echo "validateVoucher.php";?>' method="POST">
-                <div class="form-group row row-no-gutters">
-                    <div class="col-sm-3">
-                        <label class="label label-default">Scan QRCode or Type in here :</label>
-                        <div class="row-no-gutters">
-                            <input class=" form-control text-primary inline " type="input" name="serialcode" id="serialcode" value="" placeholder="Serial Code" />
-                            <input class="inline btn btn-info btn-block" style=' align-self: center ' type="submit" name="submitSerialCode" id="submitSerialCode" value="Submit"/>
+<?php
+if (isset($vouchertype)) {
+    ?>
+                <form action='validateVoucher.php' method="POST">
+                    <div class="form-group row row-no-gutters">
+                        <div class="col-sm-3">
+                            <label class="label label-default">Scan QRCode or Type in here :</label>
+                            <div class="row-no-gutters">
+                                <input class=" form-control text-primary inline " type="input" name="serialcode" id="serialcode" value="" placeholder="Serial Code" />
+                                <input class="inline btn btn-info btn-block" style=' align-self: center ' type="submit" name="submitSerialCode" id="submitSerialCode" value="Submit"/>
+                            </div>
                         </div>
                     </div>
+                    <input type='hidden' name='vouchertype' id='vouchertype' value="<?php echo $vouchertype; ?>" />
+                </form>
+    <?php
+}
+if (isset($voucherUpdMsg)) {
+    ?>
+                <div class="form-group row row-no-gutters">
+                    <label class="alert alert-block alert-dismissable"><?php echo $voucherUpdMsg; ?></label>
                 </div>
-                <input type='hidden' name='vouchertype' id='vouchertype' value="<?php echo $vouchertype; ?>" />
-            </form>
-                <?php
-        }
-        if (isset($voucherUpdMsg)){
-            ?>
-            <div class="form-group row row-no-gutters">
-                <label class="alert alert-block alert-dismissable"><?php echo $voucherUpdMsg; ?></label>
-            </div>
-            <?php
-        }
-        ?>
-            
+    <?php
+}
+?>
+
         </div>
-        
-        
+
+
     </body>
-    </body>
+</body>
 </html>
