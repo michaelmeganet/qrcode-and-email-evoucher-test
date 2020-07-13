ae33c69f71bba81cc81caf8f77e2b7bc126dcae1
Claudio Christyo <mosint.nagant@gmail.com>
2020-07-13 08:29:17 +0700

    modified parameters for Evoucher

--- testBody.php
+++ testBody.php
@@ -1,19 +1,15 @@
 <?php
 include_once "header.php";
-if($_SESSION['post']){
-    $postdata = $_SESSION['post'];
-    $vouchertype = "RM".$postdata['radioRM'];
-}
 ?>
 
 <div class='container'>
     <h4>Thank you for being a member of our Restaurant</h4>
     <br>
-    We have provided you with voucher with amount of <?phpecho $vouchertype; ?><br>
+    We have provided you with a voucher.<br>
     <!--Please show the attached file to our staff for confirmation!-->
     Please show this voucher to our staff for confirmation!
 </div>
 <?php
-include_once 'evoucher-generate.php';
+include 'evoucher-generate.php';
 ?>
     
