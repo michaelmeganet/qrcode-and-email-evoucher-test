3d8d30165060d0f74988dd9619644f15a335d363
Claudio Christyo <mosint.nagant@gmail.com>
2020-07-17 16:42:32 +0700

    Changed namespace to CRUD\Customer\Update Set all textbox into readonly

--- CRUD-DetailsCustomer.php
+++ CRUD-DetailsCustomer.php
@@ -1,12 +1,12 @@
 <?php
-namespace CRUD\Customer;
+namespace CRUD\Customer\Details;
 include_once 'header.php';
 require 'CRUD-functions.php';
 require 'customer.inc.php';
 
 if (isset($_GET['cid'])){
     $cid = $_GET['cid'];
-    $customerDetail = getCustomerListDetail($cid);
+    $customerDetail = \CRUD\Customer\getCustomerListDetail($cid);
     extract($customerDetail,EXTR_PREFIX_ALL,'dtl'); //extracts the array as $dtl_keyArray
 }else{
     die('Cannot reach the page this way, <a href="view_customerCRUD.php">Please Try Again</a>.');
@@ -37,18 +37,18 @@ and open the template in the editor.
                 <input class="form-control" style="max-width: 100px;text-align: right" type="text" id="cid" name="cid" value ="<?php echo $dtl_cid;?>" readonly/>
                 <br>
                 <label class="label label-default" style="font-size: 15px">Customer Name</label>
-                <input class="form-control" style="max-width: 100%;text-align: left" type="text" id="cus_name" name="cus_name" value='<?php echo $dtl_cus_name;?>' placeholder="Customer Name"/>
+                <input class="form-control" style="max-width: 100%;text-align: left" type="text" id="cus_name" name="cus_name" value='<?php echo $dtl_cus_name;?>' placeholder="Customer Name" readonly/>
                 <br>
                 <label class="label label-default" style="font-size: 15px">Address</label>
-                <input class="form-control" style="max-width: 100%;text-align: left" type="text" id="address1" name="address1" value='<?php echo $dtl_address1;?>' placeholder="Address1"/>
-                <input class="form-control" style="max-width: 100%;text-align: left" type="text" id="address2" name="address2" value='<?php echo $dtl_address2;?>' placeholder="Address2"/>
-                <input class="form-control" style="max-width: 100%;text-align: left" type="text" id="address3" name="address3" value='<?php echo $dtl_address3;?>' placeholder="Address3"/>
+                <input class="form-control" style="max-width: 100%;text-align: left" type="text" id="address1" name="address1" value='<?php echo $dtl_address1;?>' placeholder="Address1" readonly/>
+                <input class="form-control" style="max-width: 100%;text-align: left" type="text" id="address2" name="address2" value='<?php echo $dtl_address2;?>' placeholder="Address2" readonly/>
+                <input class="form-control" style="max-width: 100%;text-align: left" type="text" id="address3" name="address3" value='<?php echo $dtl_address3;?>' placeholder="Address3" readonly/>
                 <br>
                 <label class="label label-default" style="font-size: 15px">Email</label>
-                <input class="form-control" style="max-width: 100%;text-align: left" type="text" id="email" name="email" value='<?php echo $dtl_email;?>' placeholder="E-Mail"/>
+                <input class="form-control" style="max-width: 100%;text-align: left" type="text" id="email" name="email" value='<?php echo $dtl_email;?>' placeholder="E-Mail" readonly/>
                 <br>
                 <label class="label label-default" style="font-size: 15px">Status</label>
-                <input class="form-control" style="max-width: 100%;text-align: left" type="text" id="status" name="status" value='<?php echo $dtl_status;?>' placeholder="Status"/>
+                <input class="form-control" style="max-width: 100%;text-align: left" type="text" id="status" name="status" value='<?php echo $dtl_status;?>' placeholder="Status" readonly/>
                 <br>
             </div>
                 <div class='form-group row col-sm-4' style='text-align:left'>
