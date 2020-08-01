8e3744ea56971c34d37e345bccbc21c9295de80b
michaelmeganet <phhfirewall@gmail.com>
2020-08-01 14:55:46 +0800

    update repo after merge, add back voucherlog/voucherlog-functions.php and form_mailCustomer.php in git repo

---
+++ voucherlog/voucherlog-functions.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace voucher\Log;
+
+<<<<<<< HEAD
+
+// require 'class/variables.inc.php';
+=======
+include_once 'class/dbh.inc.php';
+include_once 'class/variables.inc.php';
+>>>>>>> refs/remotes/origin/develop
+
+use SQL;
+
+function getEVoucherListFilter($filterby = 'none', $filterval = 'none', $startLimit = 'none', $offset = 'none') {
+    if ($filterby != 'none' && $filterval != 'none') {
+        switch ($filterby) {
+            case 'cusname':
+                $whereClause = " WHERE cus_name LIKE '%$filterval%'";
+                break;
+            case 'user':
+                $whereClause = " WHERE userid LIKE '%$filterval%'";
+                break;
+        }
+    } else {
+        $whereClause = '';
+    }
+    #echo "startLimit = $startLimit, offset = $offset<br>";
+    if ($startLimit !== 'none' && $offset !== 'none') {
+        #echo "limit1<br>";
+        $limitClause = "LIMIT $startLimit, $offset";
+    } else {
+        # echo "limit2<br>";
+        $limitClause = '';
+    }
+    $qr = " SELECT sid, customers.cus_name, instanceid, valvoucher, datecreate, expiredate, dateredeem, void, userid FROM evoucher_serial
+            LEFT JOIN customers
+            ON customers.cid = evoucher_serial.cid
+            $whereClause  
+             ORDER BY datecreate DESC 
+            $limitClause";
+    #echo "qr = $qr<br>";
+    $objSQL = new SQL($qr);
+    $result = $objSQL->getResultRowArray();
+    return $result;
+}
+
+function getPPVoucherListFilter($filterby = 'none', $filterval = 'none', $startLimit = 'none', $offset = 'none') {
+    if ($filterby != 'none' && $filterval != 'none') {
+        switch ($filterby) {
+            case 'user':
+                $whereClause = " WHERE name LIKE '%$filterval%'";
+                break;
+        }
+    } else {
+        $whereClause = '';
+    }
+    #echo "startLimit = $startLimit, offset = $offset<br>";
+    if ($startLimit !== 'none' && $offset !== 'none') {
+        #echo "limit1<br>";
+        $limitClause = "LIMIT $startLimit, $offset";
+    } else {
+        # echo "limit2<br>";
+        $limitClause = '';
+    }
+    $qr = " SELECT psid, runningno, valvoucher, datecreate, expiredate, dateredeem, void,userid FROM preprint_serial
+            $whereClause  
+             ORDER BY datecreate DESC 
+            $limitClause";
+    #echo "qr = $qr<br>";
+    $objSQL = new SQL($qr);
+    $result = $objSQL->getResultRowArray();
+    return $result;
+}
+
+function countEVoucherListFilter($filterby = 'none', $filterval = '') {
+    if ($filterby != 'none' && $filterval != '') {
+        switch ($filterby) {
+            case 'cusname':
+                $whereClause = " WHERE cus_name LIKE '%$filterval%'";
+                break;
+            case 'user':
+                $whereClause = " WHERE userid LIKE '%$filterval%'";
+                break;
+        }
+    } else {
+        $whereClause = '';
+    }
+    $qr = " SELECT COUNT(*) FROM evoucher_serial
+            LEFT JOIN customers ON customers.cid = evoucher_serial.cid
+            $whereClause  ";
+    $objSQL = new SQL($qr);
+    $result = $objSQL->getRowCount();
+    return $result;
+}
+
+function countPPVoucherListFilter($filterby = 'none', $filterval = '') {
+    if ($filterby != 'none' && $filterval != '') {
+        switch ($filterby) {
+            case 'user':
+                $whereClause = " WHERE userid LIKE '%$filterval%'";
+                break;
+        }
+    } else {
+        $whereClause = '';
+    }
+    $qr = " SELECT COUNT(*) FROM preprint_serial
+            $whereClause  ";
+    $objSQL = new SQL($qr);
+    $result = $objSQL->getRowCount();
+    return $result;
+}
+?>
