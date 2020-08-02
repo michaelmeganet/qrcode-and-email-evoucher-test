70a98fa44a7a812cd38477b3e8a940684ede0ed7
michaelmeganet <phhfirewall@gmail.com>
2020-08-02 14:42:04 +0800

    update testsendmail.php for return response

--- testsendmail.php
+++ testsendmail.php
@@ -4,5 +4,12 @@ $to_email = 'cct3000@gmail.com';
 $subject = 'Testing PHP Mail mail() function from webfaction';
 $message = 'This mail is sent using the PHP mail function';
 $headers = 'From: noreply@company.com';
-mail($to_email, $subject, $message, $headers);
+//mail($to_email, $subject, $message, $headers);
+
+
+if (mail($to_email, $subject, $message, $headers)) {
+    echo "Message successfully sent!";
+} else {
+    echo "Message delivery failed...";
+}
 ?>
