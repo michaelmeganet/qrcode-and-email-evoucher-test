2d5825f2591afe09f8a0777ba3fe7f567f407f70
michaelmeganet <phhfirewall@gmail.com>
2020-08-03 07:32:31 +0800

    update sendmailpear.php for got testing only update #1

---
+++ sendmailpear.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once "Mail.php";
+
+$from_addr = "Me <meganet001@gmail.com>";
+$to = "user <cct3000@gmail.com>";
+$subject = "Hello!";
+$body = "Dear Team, here is my message text.";
+
+$headers = array("From" => $from_addr,
+    "To" => $to,
+    "Subject" => $subject);
+$smtp = Mail::factory("smtp", array('host' => "smtp.webfaction.com",
+            'auth' => true,
+            'username' => "meganetinfo",
+            'password' => "@#mega6636"));
+
+$mail = $smtp->send($to, $headers, $body);
+?>
